5940b353899b5690b0ab3ebe897f8ee8fba4db9b
jason <jasonysh419@gmail.com>
2021-01-06 23:32:11 +0800

    [leetcode-02]Add two numbers

---
+++ leetcode-java/array/AddTwoNumbers.java
@@ -0,0 +1,40 @@
+/**
+ * @program:leetcode-java
+ * @description:
+ * 2个逆序的链表，要求从低位开始相加，得出结果也逆序输出，返回值是逆序结果链表的头结点。
+ * https://leetcode.com/problems/add-two-numbers/
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ * @author:JasonYu
+ * @date:2021-01-02 20-50
+ **/
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+}
---
+++ leetcode-java/utils/ListNode.java
@@ -0,0 +1,14 @@
+/**
+ * @program:leetcode-java
+ * @description:
+ * @author:JasonYu
+ * @date:2021-01-02 21-00
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
